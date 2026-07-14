7158962094ca42adc03ef57645ffae1297de1ba4
Krishna Chhikara <krishnachhikara47@gmail.com>
2026-07-14 13:57:30 +0530

    feat: implement product and cart modules

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,7 @@
 from app.api.v1.auth import router as auth_router
 from fastapi import FastAPI
-<<<<<<< HEAD
-=======
 from app.api.v1.test import router as test_router
 from app.api.v1.product import router as product_router
->>>>>>> 4e815fb (feat: implement product and cart modules)
 
 app = FastAPI()
 
@@ -12,8 +9,6 @@ app.include_router(
     auth_router,
     prefix="/api/v1/auth",
     tags=["Authentication"]
-<<<<<<< HEAD
-=======
 )
 
 # app.include_router(test_router)
@@ -23,5 +18,4 @@ app.include_router(
     product_router,
     prefix="/api/v1/products",
     tags=["Products"]
->>>>>>> 4e815fb (feat: implement product and cart modules)
 )
